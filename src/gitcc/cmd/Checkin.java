78d3d24948395fd1fc49f9a73f5a12f4a6011fa4
Charles O'Farrell <charleso@charleso.org>
2009-06-26 07:09:05 +1000

    Make checkin get git log first to avoid slow Clearcase operations

--- src/gitcc/cmd/Checkin.java
+++ src/gitcc/cmd/Checkin.java
@@ -1,5 +1,7 @@
 package gitcc.cmd;
 
+import java.util.List;
+
 import gitcc.cc.Transaction;
 import gitcc.git.GitCommit;
 
@@ -7,8 +9,11 @@ public class Checkin extends Command {
 
 	@Override
 	public void execute() throws Exception {
+		List<GitCommit> log = git.log(config.getCI() + "..");
+		if (log.isEmpty())
+			return;
 		cc.update();
-		for (GitCommit c : git.log(config.getCI() + "..")) {
+		for (GitCommit c : log) {
 			init(new Transaction(c, git.getStatuses(c))).process();
 		}
 		cc.deliver();
